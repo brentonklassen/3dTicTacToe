93e3806c063b4ab93c0e2c36298d789f6cd2e5bf
Brenton Klassen <midwestmicro@live.com>
2014-11-21 22:20:52 -0600

    Worked on equal function

--- ttt.js
+++ ttt.js
@@ -638,7 +638,7 @@ function getBestMirror(cell){
 }
 
 function equal(a,b){
-	return a[0] == b[0] && a[1] == b[1] && a[2] == b[2];
+	return a && b && a[0] == b[0] && a[1] == b[1] && a[2] == b[2];
 }
 
 function canWin(player){
